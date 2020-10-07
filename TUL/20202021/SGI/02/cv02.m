db13dde242d215422a45a20fec03911a1c39e413
aliaksei.kalosha <aliaksei.kalosha@tul.cz>
2020-10-07 20:21:31 +0200

    fix SGI/02/cv02.m

--- TUL/20202021/SGI/02/cv02.m
+++ TUL/20202021/SGI/02/cv02.m
@@ -4,14 +4,17 @@ close all
 Fs = 20;
 T = 2;
 t = 0:1/Fs:T-1/Fs;
-x = graf(2,t,4,pi/3,0);
-y = graf(4,t,2,pi/4,0);
+sig1 = graf(2,t,4,pi/3,0);
+sig2 = graf(4,t,2,pi/4,0);
+gcf = figure();
 subplot(3,1,1)
-stem(t,x)
+stem(t,sig1)
 subplot(3,1,2)
-stem(t,y)
+stem(t,sig2)
 subplot(3,1,3)
-stem(t,x+y)
+sig12 = sig1 + sig2;
+stem(t,sig12)
+saveas(gcf, 'output.png')
 
 function r = graf(A,t,f,fi,M)
     r = A*cos(2*pi*t*f+fi)+M;
