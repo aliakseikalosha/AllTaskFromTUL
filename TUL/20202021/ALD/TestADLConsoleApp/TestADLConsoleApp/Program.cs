0a5ea408a7465dca7a791a9c18c21b4c1423c978
Aliaksei Kalosha <kalosha@skeleton.cz>
2020-10-12 12:17:15 +0200

    edit SGI/02/bonus/musicScript.m ALD new TestConsoleApplication

--- TUL/20202021/ALD/TestADLConsoleApp/TestADLConsoleApp/Program.cs
+++ TUL/20202021/ALD/TestADLConsoleApp/TestADLConsoleApp/Program.cs
@@ -1,17 +1,27 @@
-﻿//using System;
+﻿using System;
 
-//namespace TestADLConsoleApp
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            int n = int.Parse(Console.ReadLine());
-//            for (int i = 0; i < n; i++)
-//            {
-//                Console.WriteLine("Hello World!");
-//            }
-//        }
-//    }
-//}
-class p{static void Main(){var b=System.Numerics.BigInteger.Pow(100,1000)*2;var e=b/2;for(int i=3333;i-->0;e=(e+b/e)/2);System.Console.Write((""+e).Insert(1,"."));}}
+namespace TestADLConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string s = "";
+            for (int i = 1; i <= n; i++)
+            {
+                s += $"{i}:";
+                for (int j = 1; j <= i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        s += $" {j},";
+                    }
+                }
+                s = s.Substring(0, s.Length - 1) + "\n";
+            }
+            Console.WriteLine(s);
+        }
+    }
+}
+//class p{static void Main(){var b=System.Numerics.BigInteger.Pow(100,1000)*2;var e=b/2;for(int i=3333;i-->0;e=(e+b/e)/2);System.Console.Write((""+e).Insert(1,"."));}}
--- TUL/20202021/SGI/02/bonus/musicScript.m
+++ TUL/20202021/SGI/02/bonus/musicScript.m
@@ -1,69 +1,49 @@
-% hudba https://musescore.com/user/27749405/scores/6290245
+% hudba https://musescore.com/user/10444271/scores/2338126
 clc
 clear all
 close all
 
 Fs = 16000;
-bpm = 90; 
+bpm = 160;
 volume = 0.5;
-shiftNote = -1;
-shiftDozvSec = 0.033;
-dzvVolume = 0.3;
+shiftAct = 0;
+shiftTone = 0;
+
+shiftDozvSec = 0;%0.033;
+dzvVolume = 0.1;
 
 q = 2^(1/12);
 q12 = q^12;
 
-a1 = 440;
-f1 = a1/q/q;
-e1 = f1/q;
-h1 = a1*q;
-
+g1  = 391.995;
+g2  = g1*q12;
+pau = 0;
+e1 = 329.628;
 e2 = e1*q12;
-a2 = a1*q12;
-c2 = a2*q*q;
-f2 = f1*q12;
-h2 = h1*q12;
+e2niz = e2/q;
 
+f2 = 698.5;
+f2vys = f2*q;
+
+a1	= 440;
+alvys = a1*q;
+a1niz =	a1/q;
+a2 = 440*q12;		
+
+h1 = 493.883;
+h1vys = 466.2;
+
+c2 = 523.251;
 c3 = c2*q12;
-ceses2 = c2/q/q;
-pause = 0;
 
-notesLenght = [...
-    1/16,1/16,1/16,1/16,...
-    1/16,1/16,1/16,1/16,...
-    1/16,1/16,...
-    1/4, 1/4, 1/4,...
-    1/8, 1/16, 1/4,...
-    1/8,1/8,1/8,1/8, 1/16,1/16,1/16,...
-    1/8,1/16,1/8,1/4,1/8,1/8,...
-    1/2, 3/8, 1/4,1/4,1/4, 1/8,...
-    1/4, 1/16,1/16,1/16,1/16, 1/16,1/16, 3/8,...
-    1/4, 1/8,1/16,1/16,...
-    1/16,1/16,1/16,1/16,...
-    1/8,1/16,1/16,...
-    1/16,1/16,1/16,1/16,...
-    1/16,1/16,1/16,1/16,...
-    1/16,1/16,1/4,1/4];
+d2 = 587.330;
 
-notesFreq = [...
-    e1,f1,a1,h1,...
-    ceses2,c2,e2,c2,...
-    e2,f2,...
-    a2,f2,ceses2,...
-    h1,a1,f1,...
-    f2,f2,f2,e2, h1,c2,e2,...
-    f2,a2,h2,c3,h2,a2,...
-    f2, c3, h2,a2,f2, e2,...
-    c2, f2,e2,c2,c2, h1,a1, c2,...
-    h1, a1,f1,e1,...
-    f1,a1,h1,ceses2,...
-    c2,e2,c2,...
-    e2,c2,e2,a1,...
-    h1,ceses2,c2,e2,...
-    c2,h1,a2,pause];
+notesFreq = [e2,e2,pau,e2,pau,c2,e2,g2,pau,g1,pau,c2,pau,g1,pau,e1,pau,a1,pau,h1,pau,h1vys,a1,g1,e2,g2,a2,f2,g2,pau,e2,pau,c2,d2,h1,pau,pau,g2,f2vys,f2,e2niz,pau,e2,pau,a1niz,alvys,c2,pau,a1,c2,e2,pau,g2,f2vys,f2,e2niz,pau,e2,pau,c3,pau,c3,c3,pau,pau,g2,f2vys,f2,e2niz,pau,e2,pau,a1niz,alvys,c2,pau,a1];
+notesLenght = [0.125,0.125,0.125,0.125,0.125,0.125,0.25,0.25,0.25,0.25,0.25,0.25,0.125,0.125,0.25,0.25,0.125,0.125,0.125,0.125,0.125,0.125,0.25,0.25,0.25,0.25,0.25,0.125,0.125,0.125,0.125,0.125,0.125,0.125,0.125,0.25,0.25,0.125,0.125,0.125,0.125,0.125,0.125,0.125,0.125,0.25,0.25,0.125,0.125,0.125,0.125,0.25,0.125,0.125,0.125,0.125,0.125,0.125,0.125,0.125,0.125,0.125,0.25,0.25,0.25,0.125,0.125,0.125,0.125,0.125,0.125,0.125,0.125,0.125,0.125,0.125,0.125];
+%notesLenght = [1/4,1/4,1/8,1/8,1/16,1/16,1/16,1/16,4/8,1/8,1/8,(2/3)/8,(2/3)/8,(2/3)/8,4/8,1/8,1/8,(2/3)/8,(2/3)/8,(2/3)/8,3/16,1/16,1/2,1/4,1/8,1/16,1/16,1/2,1/8,1/8,1/8,1/16,1/16,1/2,1/8,1/8,1/8,1/16,1/16,1/2,1/4,1/8,1/16,1/16,1/8,1/16,1/16,1/8,1/16,1/16,1/8,1/16,1/16,1/4,1/4,1/8,1/8,1/16,1/16,1/16,1/16,4/8,1/8,1/8,(2/3)/8,(2/3)/8,(2/3)/8,3/4,1/4,1/4,1/2,1/4,3/4,1/4,1/4,1/2,1/4,3/4,1/4,1/4,1/2,1/4,3/4,1/4,1/4,1/2,1/4,1/8,1/16,1/16,1/2,1/4,1/8,1/16,1/16,1/8,1/16,1/16,1/8,1/16,1/16,1/8,1/16,1/16];
+%notesFreq = [h1,f1,f1,h1,h1,c2,dis2,e2,f2,f2,f2,f2,g2,a2,h2,h2,h2,h2,a2,g2,a2,g2,f2,f2,e2,e2,f2,g2,f2,e2,d2,d2,e2,f2,e2,d2,c2,c2,dis2,feses2,g2,f2,f1,f1,f1,f1,f1,f1,f1,f1,f1,g1,a1,h1,f1,f1,h1,h1,c2,dis2,e2,f2,f2,f2,f2,g2,a2,h2,d3,c3,a2,f2,g2,h2,ais2,f2,f2,g2,h2  ,ais2,f2,his1,e2,g2,f2,d2,h1,c2,c2,dis2,eis2,gis2,f2,f1,f1,f1,f1,f1,f1,f1,f1,f1,g1,a1];
 
-%apply forgotten ## and shift
-notesFreq=notesFreq * (2^shiftNote);
+notesFreq=notesFreq * (2^shiftAct)*(q^shiftTone);
 
 timeTotal=0;
 music = [];
@@ -77,9 +57,11 @@ for i=1:size(notesLenght,2)
 end
 
 musicDzv = music + [zeros(1,shiftDozvSec*Fs),music(1:size(music,2)-shiftDozvSec*Fs)] .* dzvVolume;
-%sound(musicDzv);
+musicDzv = musicDzv/max(musicDzv)*volume;
+%pause
+sound(musicDzv,Fs);
 %sound(musicTrian);
-audiowrite('lazaretto.wav',music,Fs);
-audiowrite('lazarettoDzv.wav',musicDzv,Fs);
-%audiowrite('lazarettoTrian.wav',musicTrian,Fs);
+%audiowrite('zelda.wav',music,Fs);
+audiowrite('zeldaDzv.wav',musicDzv,Fs);
+%audiowrite('zeldaTrian.wav',musicTrian,Fs);
 
