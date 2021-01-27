4beb0292c0853beac175808564de4250a2379ac5
Aliaksei Kalosha <aliaksei.kalosha@tul.cz>
2021-01-27 22:07:47 +0100

    updated exam SQL DBS

--- TUL/20202021/PRJC/prjc-cpp-exam/main.cpp
+++ TUL/20202021/PRJC/prjc-cpp-exam/main.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
 
+
 int main() {
-    int* a = (int*)malloc(sizeof(int));
-    a = new int[2];
-    *a = 10;
-    std::cout << "Hello, World! address :"<< a <<" : "<< *a << std::endl;
-    delete a;
-    return 0;
+
+
 }
